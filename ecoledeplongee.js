493fc0b1dfd27e1ca003d287e4c2a02ba42593c4
jcc <jccollart@outlook.be>
2023-04-26 16:00:21 +0200

    test2

--- ecoledeplongee.js
+++ ecoledeplongee.js
@@ -4,15 +4,10 @@ document.querySelectorAll("li").forEach((niveau) => {
   niveau.addEventListener("mouseover", () => {
     card = document.createElement("div");
     card.className = "card";
-    card.innerHTML = "heelo";
+    card.innerHTML = "hello";
 
     niveau.append(card);
     // console.log(card);
-    niveau.addEventListener("mousemove", (e) => {
-      console.log(e.pageY);
-      card.style.left = e.pageX - 4 + "px";
-      card.style.top = e.pageY - 3 + "px";
-    });
   });
   niveau.addEventListener("mouseout", () => {
     card.remove();
